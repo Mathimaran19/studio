6178440382ea9b7718e618fefae2bbf35d405bb0
Mathimaran19 <mathimaran1997@gmail.com>
2023-03-31 20:56:54 +0530

    3rd commit

--- customer.php
+++ customer.php
@@ -1,37 +1,27 @@
-<?php>
-   /*$firstname = $_POST[' firstname'];
-   $lastname = $_POST['lastname'];
-   $birthday= $_POST['birthday'];
-   $gender = $_POST['gender'];
-   $email = $_POST['email'];
-   $phoneNumber = $_POST['phone number'];
-   $options= $_POST['options'];
+<?php
 
-//Data base connection
-    
-    $conn = new mysqli('localhost', 'root', 'Maran"1997', 'girrfe');
-    if($conn->connect_error){
-        die('Connection Failed: '.$conn->connect_error');
-    } else{
-        $stmt = $conn->prepare("inser into Customers(firstname, lastname, birthday, gender,email,phoneNumber, options) values(?,?,?,?,?,?,?)");
-        $stmt->bind_param("sssssss",$firstname,$lastname,$birthday,$gender,$email,$phoneNumber,$options);
-        $stmt->execute();
-        echo "registration Success...";
-        $stmt->close();
-        $conn->close();
-    }
-*/
+ echo $_POST['firstname'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $birthday = $_POST['birthday'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phoneNumber = $_POST['PhoneNumber'];
+    $password = $_POST['password'];
+    $options = $_POST['options'];
 
-$servername = "localhost";
-$username = "root";
-$password = "Maran@1997";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-</php>
+   $conn = new mysqli('localhost', 'root', ' ', 'customers');
+   if($conn->connect_error){
+    die('connection failed:' .$conn->connect_error);
+   }
+   else{
+    $stmt = $conn->prepare("insert into Customers(firstname,lastname,birthday,gender,email,phoneNumber,password,opthions)
+    values(?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("ssssssss",$firstname,$lastname,$birthday,$gender,$email,$phoneNumber,$password,$options);
+    $stmt->execute();
+    echo "registration successs";
+    $stmt->close();
+    $conn->close();
+   }
+?>
